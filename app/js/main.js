f06618816df423bec8f9a8e60a9facb623726265
ekaterynakravchuk <ekateryna.kravchuk@gmail.com>
2021-09-16 20:55:36 +0300

    products

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,9 @@
 $(function(){
 
     $('.products__slider').slick({
-
+        prevArrow: '<button class="slider-btn slider-left"><svg width="10" height="18" viewBox="0 0 10 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M9.21839 1L1 9L9.21839 17" stroke="white"/></svg></button>',
+        nextArrow: '<button class="slider-btn slider-right"><svg width="10" height="18" viewBox="0 0 10 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0.78161 17L9 9L0.78161 1" stroke="white"/></svg></button>',
+        infinite: false,
     });
 
 
